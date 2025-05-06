54cfd3d7bd46a2c67a804596f8063f8264010f0b
Felipe Carvalho <felipe_pc7@hotmail.com>
2025-05-06 15:16:07 -0300

    refactor: importing sidebar into page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { FlowEditor } from "./components/flow-editor";
+import { Sidebar } from "./components/sidebar";
 
 export default function Home() {
-  return <FlowEditor />;
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 h-full">
+        <FlowEditor />
+      </div>
+    </div>
+  );
 }
